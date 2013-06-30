63e627ec72ba4833cbbd586076c43e4b30707d3c
Zak <SonZohan@gmail.com>
2013-06-29 22:03:11 -0700

    Changed from Pin3 to Pin2 (the interrupt pin)

--- TemporalChronometer/SleepMode/SleepMode.ino
+++ TemporalChronometer/SleepMode/SleepMode.ino
@@ -1,27 +1,6 @@
 /*
-  Button
- 
- Turns on and off a light emitting diode(LED) connected to digital  
- pin 13, when pressing a pushbutton attached to pin 2. 
- 
- 
- The circuit:
- * LED attached from pin 13 to ground 
- * pushbutton attached to pin 2 from +5V
- * 10K resistor attached to pin 2 from ground
- 
- * Note: on most Arduinos there is already an LED on the board
- attached to pin 13.
- 
- 
- created 2005
- by DojoDave <http://www.0j0.org>
- modified 30 Aug 2011
- by Tom Igoe
- 
- This example code is in the public domain.
- 
- http://www.arduino.cc/en/Tutorial/Button
+  SleepMode
+  Proof of concept that demonstrates sleep mode on the ATTiny
  */
 
 // constants won't change. They're used here to 
@@ -41,7 +20,7 @@
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, 0, NEO_GRB + NEO_KHZ800);
 
 // Button pin information
-const int buttonPin = 3;     // the number of the pushbutton pin
+const int buttonPin = 2;     // the number of the pushbutton pin
 int buttonState = 0;         // variable for reading the pushbutton status
 
 #define BODS 7                   //BOD Sleep bit in MCUCR
